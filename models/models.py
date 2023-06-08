2f7e7e43d96793b1ec2a136c6be8c4a092d5198d
Cesar250101 <Cesar250101@outlook.com>
2023-06-08 16:06:37 -0400

    Kilometraje

--- models/models.py
+++ models/models.py
@@ -7,5 +7,7 @@ class OrdenReparación(models.Model):
     _inherit = 'repair.order'
     
     vehicle_id = fields.Many2one(comodel_name='fleet.vehicle', string='Vehículo')
+    kilometraje = fields.Integer(string='Kilometraje')
 
+    
 
